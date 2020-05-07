b36f506d56b75e10319fad6e56f588557528ab4b
zanibruno <corporate.zanin@gmail.com>
2020-05-07 15:51:11 +0100

    very basic crud action still working on them

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,34 @@
 class ItemsController < ApplicationController
+
+	def index
+		trip = Trip.find_by(id: params[:trip_id])
+		items = trip.items ? : trip.items = []
+		render json: items
+	end
+
+	# def show 
+	# 	trip = Trip.find_by(id: params[:trip_id])
+	# 	item = trip.items
+	# 	render json: item
+	# end 
+
+
+	def create 
+		trip = Trip.find_by(id: params[:trip_id])
+		item = Trip.items.build(item_params)
+		if item.save
+			render json: item
+		else 
+			flash.now[:error] = 'test create item'
+		end
+	end
+
+
+
+	private 
+
+
+	def item_params 
+		params.require(:item).permit(:name, :quantity, :trip_id)
+	end 
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,44 @@
 class TripsController < ApplicationController
+
+	def index 
+		trips = Trip.all 
+		render json: TripSerializer.new(trips)
+	end 	
+
+	# def show 
+	# 	trip = Trip.find_by(id: params[:id])
+	# 	render json: trip
+	# end 
+
+	def create
+		trip = Trip.new(trip_params)
+		if trip.save
+			render json: TripSerializer(trip) 
+		else 
+			flash.now[:error] = 'test create'
+		end 
+	end 
+
+	def update 
+		@trip = Trip.find_by(id: params[:id])
+		if @trip
+		@trip.update(trip_params)
+	else 
+		flash.now[:error] = 'test update'
+		end 
+	end 
+
+
+
+
+
+	private 
+
+	def trip_params 
+		params.require(:trip).permit(:country)
+	end
+
+
+
+
 end
