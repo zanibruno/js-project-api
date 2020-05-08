2e3f6ee30e372ffc09f9c3b2829e3ae7bc3abadc
zanibruno <corporate.zanin@gmail.com>
2020-05-08 20:40:19 +0100

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :trips
+  resources :trips do 
   resources :items
+end
+	resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
