f409f1155e77c4d7dd1a7b6d2a60a62a14b79891
zanibruno <corporate.zanin@gmail.com>
2020-05-08 20:40:35 +0100

    fixed migration dumb mistake

--- db/migrate/20200507125221_create_trips.rb
+++ db/migrate/20200507125221_create_trips.rb
@@ -2,8 +2,7 @@ class CreateTrips < ActiveRecord::Migration[6.0]
   def change
     create_table :trips do |t|
 
-    	t.string :country, :item, :name
-
+    	t.string :country
       t.timestamps
     end
   end
